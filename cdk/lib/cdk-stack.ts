b669b960e5f8f097988c73a6fc97613701eb6026
Magno Machado <magnomp@gmail.com>
2023-06-23 07:19:12 -0300

    For cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,74 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
+import * as rds from "aws-cdk-lib/aws-rds";
+import { Construct } from "constructs";
+import * as apprunner from "@aws-cdk/aws-apprunner-alpha";
+import * as path from "path";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "vpc", {
+      cidr: "10.0.0.0/16",
+      natGateways: 0,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: "public-subnet-1",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: "isolated-subnet-1",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 28,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dbInstance = new rds.DatabaseInstance(this, "tn-calc-db", {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_15_3,
+      }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO
+      ),
+      credentials: rds.Credentials.fromGeneratedSecret("postgres"),
+      multiAz: false,
+      allocatedStorage: 100,
+      maxAllocatedStorage: 105,
+      allowMajorVersionUpgrade: false,
+      autoMinorVersionUpgrade: true,
+      backupRetention: cdk.Duration.days(0),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+      databaseName: "tn-calc-db",
+      publiclyAccessible: false,
+    });
+
+    const imageAsset = new DockerImageAsset(this, "ImageAssets", {
+      directory: path.join(__dirname, "../../app"),
+    });
+
+    const service = new apprunner.Service(this, "ApiService", {
+      source: apprunner.Source.fromAsset({
+        imageConfiguration: { port: 80 },
+        asset: imageAsset,
+      }),
+    });
+
+    new cdk.CfnOutput(this, "api-url", {
+      exportName: "api-url",
+      value: service.serviceUrl,
+      description: "URL to access the API",
+    });
   }
 }
