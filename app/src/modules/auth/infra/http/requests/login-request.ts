eadb58cdaf61909a80052be0390a8553b4f96188
Magno Machado <magnomp@gmail.com>
2023-07-22 01:53:12 +0000

    Swagger

--- app/src/modules/auth/infra/http/requests/login-request.ts
+++ app/src/modules/auth/infra/http/requests/login-request.ts
@@ -1,11 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsString } from 'class-validator';
 
 export class LoginRequest {
   @IsString()
   @Type(() => String)
+  @ApiProperty({
+    description: 'Username',
+  })
   username: string;
   @IsString()
   @Type(() => String)
+  @ApiProperty({
+    description: 'Password',
+  })
   password: string;
 }
